4644580baea00752e13db7dbf3eeb0ff8e4a52d0
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-19 08:06:00 +0530

    Time: 2 ms (98.45%) | Memory: 43 MB (31.44%) - LeetSync

--- Streaks/1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
+++ Streaks/1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -2,13 +2,11 @@ class Solution {
     public String smallestSubsequence(String s) {
         int n = s.length();
 
-        // Last occurrence of every character
         int[] last = new int[26];
         for (int i = 0; i < n; i++) {
             last[s.charAt(i) - 'a'] = i;
         }
 
-        // Whether a character is already in the stack
         boolean[] inStack = new boolean[26];
 
         StringBuilder stack = new StringBuilder();
@@ -16,12 +14,10 @@ class Solution {
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
 
-            // Skip duplicate characters
             if (inStack[ch - 'a']) {
                 continue;
             }
 
-            // Remove larger characters if they appear later again
             while (stack.length() > 0 &&
                    stack.charAt(stack.length() - 1) > ch &&
                    last[stack.charAt(stack.length() - 1) - 'a'] > i) {
