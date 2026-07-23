f258ccd07c90977af9032ff4deac67e41d49adeb
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-23 15:17:26 +0530

    Time: 1 ms (51.02%) | Memory: 125.3 MB (77.55%) - LeetSync

--- Streaks/3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ Streaks/3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -1,12 +1,11 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-        if (n==1 || n==2) return n;
-        int p = 0;
-        while(n != 0){
-            n = n >> 1;
-            p++;
+
+        if (n < 3) {
+            return n;
         }
-        return (int) Math.pow(2, p);
+
+        return Integer.highestOneBit(n) << 1;
     }
 }
