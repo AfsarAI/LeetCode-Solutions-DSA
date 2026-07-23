480c5102a98bcb4d9e7bd264ff4a4b437765f1a8
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-23 18:18:54 +0530

    Time: 13 ms (97.09%) | Memory: 180.4 MB (27.85%) - LeetSync

--- Streaks/403-frog-jump/frog-jump.java
+++ Streaks/403-frog-jump/frog-jump.java
@@ -1,33 +1,29 @@
 class Solution {
-
+    private Boolean[][] dp;
     public boolean canCross(int[] stones) {
-
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-
-        for (int stone : stones) {
-            map.put(stone, new HashSet<>());
-        }
-
-        map.get(0).add(0);
-
-        for (int stone : stones) {
-
-            for (int jump : map.get(stone)) {
-
-                for (int nextJump = jump - 1; nextJump <= jump + 1; nextJump++) {
-
-                    if (nextJump <= 0)
-                        continue;
-
-                    int nextStone = stone + nextJump;
-
-                    if (map.containsKey(nextStone)) {
-                        map.get(nextStone).add(nextJump);
-                    }
+        int n = stones.length;
+        if (stones[1] != 1)
+            return false;
+        dp = new Boolean[n][n + 1];
+        return dfs(1, 1, stones);
+    }
+    private boolean dfs(int index, int lastJump, int[] stones) {
+        int n = stones.length;
+        if (index == n - 1)
+            return true;
+        if (dp[index][lastJump] != null)
+            return dp[index][lastJump];
+        for (int jump = lastJump - 1; jump <= lastJump + 1; jump++) {
+            if (jump <= 0)
+                continue;
+            int nextStone = stones[index] + jump;
+            int nextIndex = Arrays.binarySearch(stones, nextStone);
+            if (nextIndex >= 0) {
+                if (dfs(nextIndex, jump, stones)) {
+                    return dp[index][lastJump] = true;
                 }
             }
         }
-
-        return !map.get(stones[stones.length - 1]).isEmpty();
+        return dp[index][lastJump] = false;
     }
 }
