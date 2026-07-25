e060f0a253053e2ad670067bb54bd0ae242bf5f6
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-25 17:29:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (23.31%) - LeetSync

---
+++ Streaks/3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max = -1;
+        int secMax = max;
+        int num = n;
+
+        while(num > 0){
+            int digit = num % 10;
+            if (digit >= max){
+                secMax = max;
+                max = digit;
+            } else if (digit > secMax) {
+                secMax = digit;
+            }
+            num /= 10;
+        }
+        return max * secMax;
+    }
+}
