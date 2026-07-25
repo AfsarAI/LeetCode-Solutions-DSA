a87562e743a1b6819ea3e8a6e7945f8790014c1c
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-25 18:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (29.45%) - LeetSync

--- DP/62-unique-paths/unique-paths.java
+++ DP/62-unique-paths/unique-paths.java
@@ -7,7 +7,7 @@ class Solution {
         return dfs(m-1, n-1, dp);
     }
     private int dfs(int row, int col, int[][] dp){
-        if (row == 0 && col == 0) return 1;
+        if (row == 0 && col == 0 && dp[row][col] != 0) return 1;
 
         if (dp[row][col] != -1) return dp[row][col];
         int up = 0, left = 0;
