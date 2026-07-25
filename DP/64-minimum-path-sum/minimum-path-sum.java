3b6c560f92751c42e40a042979c3e2f46b437760
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-25 18:37:32 +0530

    Time: 1 ms (99.93%) | Memory: 49.9 MB (94.53%) - LeetSync

--- DP/64-minimum-path-sum/minimum-path-sum.java
+++ DP/64-minimum-path-sum/minimum-path-sum.java
@@ -10,26 +10,13 @@ class Solution {
     }
     private int dfs(int row, int col, int[][] mat, int[][] dp){
         if (row == 0 && col == 0) return mat[row][col];
+        if (row < 0 || col < 0)
+            return Integer.MAX_VALUE;
 
         if (dp[row][col] != -1) return dp[row][col];
-
-        int up = -1, left = -1;
-        if (row - 1 >= 0) {
-            up = mat[row][col] + dfs(row-1, col, mat, dp);
-        }
-        if (col - 1 >= 0){
-            left = mat[row][col] + dfs(row, col-1, mat, dp);
-        }
-
-        int min = Integer.MAX_VALUE;
-        if (up != -1 && left != -1){
-            min = Math.min(up, left);
-        } else if (up == -1){
-            min = left;
-        } else {
-            min = up;
-        }
-
-        return dp[row][col] = min;
+        
+        int up = dfs(row - 1, col, mat, dp);
+        int left = dfs(row, col - 1, mat, dp);
+        return dp[row][col] = mat[row][col] + Math.min(up, left);
     }
 }
