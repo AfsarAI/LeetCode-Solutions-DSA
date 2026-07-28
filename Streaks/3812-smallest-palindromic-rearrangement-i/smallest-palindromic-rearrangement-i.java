ef7a4f861dead5db08c81d23c3042ef2ff92eef6
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-07-28 09:23:48 +0530

    Time: 26 ms (75.10%) | Memory: 48.3 MB (38.15%) - LeetSync

--- Streaks/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ Streaks/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,35 +1,3 @@
-// class Solution {
-//     public String smallestPalindrome(String s) {
-//         int size = s.length();
-//         int midIndex = size / 2;
-//         String mid = "";
-//         if (size % 2 != 0)
-//             mid = String.valueOf(s.charAt(midIndex));
-
-//         int[] charCount = new int[26];
-
-//         for (int i = 0; i < midIndex; i++){
-//             char c = s.charAt(i);
-//             charCount[c - 'a']++;
-//         }
-
-//         StringBuilder firstHalf = new StringBuilder();
-
-//         for (int i = 0; i < 26; i++){
-//             for (int j = 0; j < charCount[i]; j++) {
-//                 firstHalf.append((char)('a' + i));
-//             }
-//         }
-
-//         StringBuilder secondHalf = new StringBuilder(firstHalf).reverse();
-
-//         StringBuilder ans = new StringBuilder();
-//         ans.append(firstHalf).append(mid).append(secondHalf);
-
-//         return ans.toString();
-//     }
-// }
-
 class Solution {
     public String smallestPalindrome(String s) {
         int size = s.length();
@@ -38,14 +6,26 @@ class Solution {
         if (size % 2 != 0)
             mid = String.valueOf(s.charAt(midIndex));
 
-        String first = s.substring(0, midIndex);
+        int[] charCount = new int[26];
 
-        char[] arr = first.toCharArray();
-        Arrays.sort(arr);
+        for (int i = 0; i < midIndex; i++){
+            char c = s.charAt(i);
+            charCount[c - 'a']++;
+        }
 
-        StringBuilder firstHalf = new StringBuilder(new String(arr));
+        StringBuilder firstHalf = new StringBuilder();
+
+        for (int i = 0; i < 26; i++){
+            for (int j = 0; j < charCount[i]; j++) {
+                firstHalf.append((char)('a' + i));
+            }
+        }
+
         StringBuilder secondHalf = new StringBuilder(firstHalf).reverse();
 
-        return firstHalf + mid + secondHalf.toString();
+        StringBuilder ans = new StringBuilder();
+        ans.append(firstHalf).append(mid).append(secondHalf);
+
+        return ans.toString();
     }
 }
