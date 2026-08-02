d7280f748d6b250178b02260ca307381695cf040
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-08-02 10:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.35%) - LeetSync

---
+++ Streaks/909-stone-game/stone-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+// class Solution {
+//     private int dfs(int[] piles, int l, int r, int[][] dp){
+//         if(l > r) return 0;
+//         if (l == r) return piles[l];
+
+//         if(dp[l][r] != -1) return dp[l][r];
+
+//         int takeL = piles[l] + Math.min(dfs(piles, l+2, r, dp), dfs(piles, l+1, r-1, dp));
+//         int takeR = piles[r] + Math.min(dfs(piles, l, r-2, dp), dfs(piles, l+1, r-1, dp));
+
+//         return dp[l][r] = Math.max(takeL, takeR);
+//     }
+//     public boolean stoneGame(int[] piles) {
+//         int n = piles.length;
+//         int total = 0;
+//         for(int i = 0; i < n; i++){
+//             total += piles[i];
+//         }
+
+//         int[][] dp = new int[n][n];
+//         for(int i = 0; i < n; i++){
+//             Arrays.fill(dp[i], -1);
+//         }
+
+//         int alic = dfs(piles, 0, n-1, dp);
+//         int bob = total - alic;
+//         return alic > bob;
+//     }
+// }
