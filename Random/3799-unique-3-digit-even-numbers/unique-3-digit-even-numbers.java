54563de7aa25a5104c4de55dfb1655d3091701bd
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-09-11 08:12:35 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (88.67%) - LeetSync

--- Random/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ Random/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,47 +1,27 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-
         int[] freq = new int[10];
-
-        // Count frequency of every digit
         for (int digit : digits) {
             freq[digit]++;
         }
-
         int ans = 0;
-
-        // 1. Choose the last digit
-        // It must be even
         for (int last = 0; last <= 8; last += 2) {
-
             if (freq[last] == 0)
                 continue;
-
-            freq[last]--;  // use one copy of last digit
-
-            // 2. Choose the first digit
-            // It cannot be 0
+            freq[last]--;
             for (int first = 1; first <= 9; first++) {
-
                 if (freq[first] == 0)
                     continue;
-
-                freq[first]--;  // use one copy
-
-                // 3. Choose the middle digit
+                freq[first]--;
                 for (int middle = 0; middle <= 9; middle++) {
-
                     if (freq[middle] > 0) {
                         ans++;
                     }
                 }
-
-                freq[first]++;  // restore first digit
+                freq[first]++;
             }
-
-            freq[last]++;  // restore last digit
+            freq[last]++;
         }
-
         return ans;
     }
 }
