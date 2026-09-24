b30488703736057f07de41f48ead21366b889db6
Mohd. Afsar <114273004+AfsarAI@users.noreply.github.com>
2026-09-24 09:37:04 +0530

    Time: 1 ms (99.69%) | Memory: 45.7 MB (24.38%) - LeetSync

---
+++ Random/3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int sum(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    public int smallestIndex(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(sum(nums[i]) == i) return i;
+        }
+        return -1;
+    }
+}
